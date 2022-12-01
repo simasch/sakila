f924974500138c5a492f8abece703e9ed6758ca0
Simon Martinelli <simon@72.services>
2022-12-01 10:42:30 +0100

    FilmsView with VScroller

--- vaadin/src/main/java/ch/martinelli/sakila/ui/views/films/FilmCard.java
+++ vaadin/src/main/java/ch/martinelli/sakila/ui/views/films/FilmCard.java
@@ -1,43 +1,44 @@
 package ch.martinelli.sakila.ui.views.films;
 
-import ch.martinelli.sakila.db.tables.records.FilmRecord;
+import ch.martinelli.sakila.db.tables.records.FilmListRecord;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.ListItem;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
 
-public class FilmCard extends Div {
+public class FilmCard extends ListItem {
 
-    public FilmCard(FilmRecord filmRecord) {
-        addClassNames("film-card", "bg-contrast-5", "flex", "flex-col", "items-start", "p-m", "rounded-l");
+    public FilmCard(FilmListRecord film, String url) {
+        addClassNames("bg-contrast-5", "flex", "flex-col", "items-start", "p-m", "rounded-l");
 
         Div div = new Div();
-        div.addClassNames("bg-contrast", "flex items-center", "justify-center", "mb-m", "overflow-hidden", "rounded-m w-full");
+        div.addClassNames("bg-contrast", "flex items-center", "justify-center", "mb-m", "overflow-hidden",
+                "rounded-m w-full");
         div.setHeight("160px");
 
         Image image = new Image();
         image.setWidth("100%");
-        image.setSrc("https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80");
-        image.setAlt(filmRecord.getTitle());
+        image.setSrc(url);
+        image.setAlt(film.getTitle());
 
         div.add(image);
 
         Span header = new Span();
         header.addClassNames("text-xl", "font-semibold");
-        header.setText(filmRecord.getTitle());
+        header.setText(film.getTitle());
 
         Span subtitle = new Span();
         subtitle.addClassNames("text-s", "text-secondary");
-        subtitle.setText(filmRecord.getReleaseYear().toString());
+        subtitle.setText(film.getActors());
 
-        Paragraph description = new Paragraph(filmRecord.getDescription());
+        Paragraph description = new Paragraph(film.getDescription());
         description.addClassName("my-m");
 
         Span badge = new Span();
         badge.getElement().setAttribute("theme", "badge");
-        badge.setText(filmRecord.getRentalRate().toString());
+        badge.setText(film.getCategory());
 
         add(div, header, subtitle, description, badge);
-
     }
 }
--- vaadin/src/main/java/ch/martinelli/sakila/ui/views/films/FilmsView.java
+++ vaadin/src/main/java/ch/martinelli/sakila/ui/views/films/FilmsView.java
@@ -1,49 +1,95 @@
 package ch.martinelli.sakila.ui.views.films;
 
-import ch.martinelli.sakila.db.tables.records.FilmRecord;
+import ch.martinelli.sakila.db.tables.records.FilmListRecord;
 import ch.martinelli.sakila.ui.views.MainLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.OrderedList;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.virtuallist.VirtualList;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
+import com.vaadin.flow.theme.lumo.LumoUtility.Display;
+import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vaadin.flow.theme.lumo.LumoUtility.JustifyContent;
+import com.vaadin.flow.theme.lumo.LumoUtility.ListStyleType;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import com.vaadin.flow.theme.lumo.LumoUtility.MaxWidth;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
+import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
 import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.vaadin.firitin.components.orderedlayout.VScroller;
 
-import static ch.martinelli.sakila.db.tables.Film.FILM;
+import static ch.martinelli.sakila.db.tables.FilmList.FILM_LIST;
 
-@AnonymousAllowed
-@PageTitle("Films")
+@PageTitle("Film List")
 @Route(value = "films", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
+@AnonymousAllowed
 public class FilmsView extends Div {
 
+    private final DSLContext dsl;
+    private final OrderedList imageContainer = new OrderedList();
+    private int offset = 0;
+
     public FilmsView(DSLContext dsl) {
-        addClassNames("films-view", "max-w-screen-lg", "mx-auto", "pb-l", "px-l");
+        this.dsl = dsl;
+
+        addClassNames("films-view");
+
+        Div content = new Div();
+        content.addClassNames(MaxWidth.SCREEN_XLARGE, Margin.AUTO, Padding.Bottom.LARGE, Padding.Horizontal.LARGE);
+
+        HorizontalLayout container = new HorizontalLayout();
+        container.addClassNames(AlignItems.CENTER, JustifyContent.BETWEEN);
+
+        VerticalLayout headerContainer = new VerticalLayout();
 
         H2 header = new H2("Popular");
-        header.addClassNames("mb-0", "mt-xl", "text-3xl");
+        header.addClassNames(Margin.Bottom.NONE, Margin.Top.XLARGE, FontSize.XXXLARGE);
+
+        Paragraph description = new Paragraph("Most popular movies");
+        description.addClassNames(Margin.Bottom.XLARGE, Margin.Top.NONE, TextColor.SECONDARY);
+
+        headerContainer.add(header, description);
 
         Select<String> sortBy = new Select<>();
         sortBy.setLabel("Sort by");
         sortBy.setItems("Popularity", "Newest first", "Oldest first");
         sortBy.setValue("Popularity");
 
-        HorizontalLayout container = new HorizontalLayout();
-        container.addClassNames("items-center", "justify-between");
-        container.add(header, sortBy);
+        container.add(headerContainer, sortBy);
 
-        VirtualList<FilmRecord> virtualList = new VirtualList<>();
-        virtualList.setHeightFull();
-        virtualList.addClassNames("gap-m", "grid", "list-none", "m-0", "p-0");
+        imageContainer.addClassNames(Gap.MEDIUM, Display.GRID, ListStyleType.NONE, Margin.NONE, Padding.NONE);
 
-        virtualList.setItems(dsl.selectFrom(FILM).stream());
-        virtualList.setRenderer(new ComponentRenderer<>(FilmCard::new));
+        loadFilms(offset);
 
-        add(container, virtualList);
+        content.add(container, imageContainer);
+
+        VScroller vScroller = new VScroller(content, Scroller.ScrollDirection.VERTICAL);
+        vScroller.setHeightFull();
+
+        vScroller.addScrollToEndListener(e -> {
+            offset += 30;
+            loadFilms(offset);
+        });
+
+        add(vScroller);
+    }
+
+    private void loadFilms(int offset) {
+        Result<FilmListRecord> films = dsl.selectFrom(FILM_LIST).offset(offset).limit(30).fetch();
+
+        for (FilmListRecord film : films) {
+            imageContainer.add(new FilmCard(film, "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        }
     }
 }

bd689d323b646ae668591ce704e2e6bbff9196ca
Simon Martinelli <simon@72.services>
2022-12-01 16:07:30 +0100

    Added Hilla project

--- hilla/src/main/java/ch/martinelli/sakila/security/SecurityConfiguration.java
+++ hilla/src/main/java/ch/martinelli/sakila/security/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package ch.martinelli.sakila.security;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,8 +15,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-//@EnableWebSecurity
-//@Configuration
+@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends VaadinWebSecurity {
 
     public static final String LOGOUT_URL = "/";
